8e3d4c72e8894aa695aa42bed03b17d3922c222c
DESKTOP-UVCAMAE\sanika <pawarsanika137@gmail.com>
2026-02-18 14:06:28 +0530

    use for loop

---
+++ 01_session_basics_python.py
@@ -0,0 +1,9 @@
+my_dict={"Name ":"Rohan",
+          "Age": "25",
+          "Gender": "Male"}
+for  key in (my_dict.keys()):
+    print (key)
+for value in (my_dict.values()):
+    print (value)
+for key, value in (my_dict.items()):
+    print (key, value)
