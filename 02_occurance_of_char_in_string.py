d36ac4603e6c577eb439c074a356ce6d67d93486
DESKTOP-UVCAMAE\sanika <pawarsanika137@gmail.com>
2026-02-18 14:25:06 +0530

    count number of character in strings

---
+++ 02_occurance_of_char_in_string.py
@@ -0,0 +1,16 @@
+#How to count number of letter in the string
+from collections import Counter
+
+
+my_string = "Hello World"
+count = 0
+for char in my_string:
+    if char =="l":
+        count += 1
+        print(count)
+
+for collection in my_string:
+    count = Counter(my_string)
+    print(count ["l"])
+
+    
