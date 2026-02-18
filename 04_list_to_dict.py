0a3a61168619c2d13304fd3a943866139da8dddb
DESKTOP-UVCAMAE\sanika <pawarsanika137@gmail.com>
2026-02-18 14:58:07 +0530

    converting list to dictionary

---
+++ 04_list_to_dict.py
@@ -0,0 +1,17 @@
+# How to convert list to dictionary
+# Two methods to convert list to dictionary
+
+# 1 zip method
+key_list = ["Name","Age","Gender"]
+value_list = ["Rohan","25","Male"]
+my_dict = dict(zip(key_list, value_list))
+print(my_dict)
+
+# 2 loop method
+i=0
+my_dict = {}
+for ikey in key_list:
+    my_dict[ikey] = value_list[i]
+    i +=1
+
+
